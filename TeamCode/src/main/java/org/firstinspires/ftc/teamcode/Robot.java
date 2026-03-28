6f3a30568edd2791cc90e7b47d0e8bac1c87d8b8
iulia-16 <toderica.iulia.maria@gmail.com>
2026-03-28 14:01:03 +0200

    first TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Robot {
+
+    private static Robot instance = null;
+    private HardwareMap hardwaremap;
+    public DcMotorEx leftFront, leftBack, rightFront, rightBack;
+
+    public static Robot getInstance(){
+        if(instance==null){
+            instance = new Robot();
+        }
+        return instance;
+    }
+
+    public void initializeHardware(final HardwareMap hardwareMap){
+        this.hardwaremap = hardwareMap;
+        leftFront = hardwaremap.get(DcMotorEx.class, "leftFront");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        rightFront = hardwaremap.get(DcMotorEx. class, "rightFront");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        leftBack = hardwaremap.get(DcMotorEx.class, "leftBack");
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        rightBack = hardwaremap.get(DcMotorEx. class, "rightBack");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp")
+public class TeleOp  extends CommandOpMode {
+    private final Robot robot = Robot.getInstance();
+    public GamepadEx gamepadEx;
+
+
+    @Override
+    public void initialize() {
+        gamepadEx = new GamepadEx(gamepad1);
+        robot.initializeHardware(hardwareMap);
+    }
+
+    @Override
+    public void run(){
+
+        robot.leftBack.setPower(gamepad1.right_stick_y);
+
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        robot.leftFront.setPower(y+x+rx);
+        robot.leftBack.setPower(y-x+rx);
+        robot.rightFront.setPower(y-x-rx);
+        robot.rightBack.setPower(y+x-rx);
+
+        telemetry.addData("TeleOp Running", "!");
+        telemetry.addData("Power", robot.leftBack.getPower());
+        telemetry.update();
+    }
+
+}
